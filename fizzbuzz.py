6f8085a4c9ccd8b86811aa1d9b785098080d771f
J.Ridgeway <sw333zy@gmail.com>
2019-04-08 15:35:25 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 #numbers for x in the range of 1 - 100 and iterating by 1
 for x in range(1,100,1):
-    #if x is a multiple 5 meaning it can be modulo by 5 and have a remander of 0 then
+    #if x is a multiple 5 meaning it can be divisable(modulo) by 5 and have a remainder of 0 then
     #it is is a multiple of that number so x%3==0 and x%5==0 
     if x%5==0 & x%3==0:
         print ('Fizzbuzz')
